dbd281db2ef95c7f21c8f94102ed0d9ae61be1e0
Levi <levipoff@yahoo.com>
2017-03-07 16:50:02 -0500

    Added main

---
+++ src/com/github/mudkipavenger/moodle_equation_creator/Main.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.github.mudkipavenger.moodle_equation_creator;
+
+/**
+ *
+ * @author levi
+ */
+public class Main {
+    
+    public static void main(String [] args)
+    {
+        System.out.println("hi");
+    }
+}
