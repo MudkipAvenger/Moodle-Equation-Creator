65a9632383f152e2e5451b0b9dc5abb735502f87
Levi <levipoff@yahoo.com>
2017-03-14 11:51:30 -0400

    Added a function node for log

---
+++ src/com/github/mudkipavenger/moodle_equation_creator/nodes/LogFunctionNode.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.github.mudkipavenger.moodle_equation_creator.nodes;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author levi
+ */
+public class LogFunctionNode extends FunctionOperatorNode {
+    
+    
+    public LogFunctionNode()
+    {
+        setFunctionName("log");
+        setNumberOfArguments(1);
+        setArgs(new ArrayList<Node>());
+    }
+    
+    @Override
+    public void traverse()
+    {
+        System.out.print(getFunctionName() + "(");
+        for(int i = 0; i < getArgs().size(); i++)
+        {
+            if(i >= 1)
+            {
+                System.out.print(", ");
+            }
+            getArgs().get(i).traverse();
+        }
+        System.out.print(")");
+    }
+}
