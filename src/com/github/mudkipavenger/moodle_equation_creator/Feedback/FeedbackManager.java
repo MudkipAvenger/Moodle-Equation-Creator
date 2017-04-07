0e71c08037e094cac14804f0e1cea19ab0d6b21f
Levi <levipoff@yahoo.com>
2017-04-07 17:08:47 -0400

    Added feedback manager class that has an arraylist that will keep track of all the added steps in the feedback

---
+++ src/com/github/mudkipavenger/moodle_equation_creator/Feedback/FeedbackManager.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.github.mudkipavenger.moodle_equation_creator.Feedback;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author levi
+ */
+public class FeedbackManager {
+    
+    private static ArrayList<Feedback> expressions = new ArrayList();
+    
+    public static void addExpression(Feedback f)
+    {
+        expressions.add(f);
+    }
+    
+    public static void addExpression(String s)
+    {
+        expressions.add(new Feedback(s));
+    }
+    
+    public static Feedback getExpression(int index)
+    {
+        return expressions.get(index);
+    }
+    
+    public static void removeExpression(int index)
+    {
+        expressions.remove(index);
+    }
+    
+    public static void removeExpression(Feedback f)
+    {
+        expressions.remove(f);
+    }
+    
+}
