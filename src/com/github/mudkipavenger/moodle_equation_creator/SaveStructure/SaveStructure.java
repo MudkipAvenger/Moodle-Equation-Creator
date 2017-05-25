3afa10484908a522a4d621c74ea5938cf767c806
Levi <levipoff@yahoo.com>
2017-05-25 14:53:47 -0400

    Added string members

--- src/com/github/mudkipavenger/moodle_equation_creator/SaveStructure/SaveStructure.java
+++ src/com/github/mudkipavenger/moodle_equation_creator/SaveStructure/SaveStructure.java
@@ -18,6 +18,12 @@ public class SaveStructure implements Serializable {
     /*
     * lots of strings from gui components
     */
+    private String ExpressionsPanel_inputTextArea_text;
+    private String ExpressionsPanel_outputTextArea_text;
+    private String QuestionPanel_questionTextArea_text;
+    private String FeedbackPanel_expressionTextArea_text;
+    private String FeedbackPanel_newWildcardNameTextField_text;
+    private String FeedbackPanel_newWildcardExpressionTextArea_text;
     
     private FeedbackManager feedbackManager;
     private WildCardManager wildcardManager;
@@ -40,6 +46,66 @@ public class SaveStructure implements Serializable {
     public void setWildCardManager(WildCardManager w)
     {
         wildcardManager = w;
+    }
+    
+    public void setExpressionsPanel_inputTextArea_text(String s)
+    {
+        ExpressionsPanel_inputTextArea_text = s;
+    }
+    
+    public String getExpressionsPanel_inputTextArea_text()
+    {
+        return ExpressionsPanel_inputTextArea_text;
+    }
+    
+    public void setExpressionsPanel_outputTextArea_text(String s)
+    {
+        ExpressionsPanel_outputTextArea_text = s;
+    }
+    
+    public String getExpressionsPanel_outputTextArea_text()
+    {
+        return ExpressionsPanel_outputTextArea_text;
+    }
+    
+    public void setQuestionPanel_questionTextArea_text(String s)
+    {
+        QuestionPanel_questionTextArea_text = s;
+    }
+    
+    public String getQuestionPanel_questionTextArea_text()
+    {
+        return QuestionPanel_questionTextArea_text;
+    }
+    
+    public void setFeedbackPanel_expressionTextArea_text(String s)
+    {
+        FeedbackPanel_expressionTextArea_text = s;
+    }
+    
+    public String getFeedbackPanel_expressionTextArea_text()
+    {
+        return FeedbackPanel_expressionTextArea_text;
+    }
+    
+    public void setFeedbackPanel_newWildcardNameTextField_text(String s)
+    {
+        FeedbackPanel_newWildcardNameTextField_text = s;
+    }
+    
+    public String getFeedbackPanel_newWildcardNameTextField_text()
+    {
+        return FeedbackPanel_newWildcardNameTextField_text;
+    }
+    
+    public void setFeedbackPanel_newWildcardExpressionTextArea_text(String s)
+    {
+        FeedbackPanel_newWildcardExpressionTextArea_text = s;
+    }
+    
+    public String getFeedbackPanel_newWildcardExpressionTextArea_text()
+    {
+        return FeedbackPanel_newWildcardExpressionTextArea_text;
     }
     
 }
